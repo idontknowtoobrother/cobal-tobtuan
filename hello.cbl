9e89528639db5493adbe8a33f8cf05689ccfd865
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-10-23 19:01:20 +0700

    data type, hello world

---
+++ hello.cbl
@@ -0,0 +1,14 @@
+      * Program Property
+       IDENTIFICATION DIVISION. 
+      * Program ID ( Name or what program doin ) 
+       PROGRAM-ID. HELLO-WORLD.
+      * Data Division      
+       DATA DIVISION.
+      * Procedure Division
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+      *    Code area
+           DISPLAY "Hello World :D".
+       END PROGRAM HELLO-WORLD.
+
+
